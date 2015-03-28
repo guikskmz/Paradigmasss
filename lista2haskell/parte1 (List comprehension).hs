4242e674e66ffef56bfdeb32e4a6d9b83ee0e33d
Guilherme Kaminski <gsantos@inf.ufsm.br>
2015-03-28 14:05:59 -0300

    L2 ordem
    start

---
+++ lista2haskell/parte1 (List comprehension).hs
@@ -0,0 +1,18 @@
+-- Nome: Guilherme Kaminski dos Santos
+-- Lista2 - Haskell
+
+
+--    PARTE1 - List comprehension
+
+
+--    1) Escreva uma função geraPotencias :: Int -> [Int], que gere uma lista com as potências de 2
+--   com expoente de n até 0, onde n é o argumento para a função. Use a sintaxe de list comprehension. 
+
+
+--    2) Escreva uma função addSuffix :: String -> [String] -> [String]
+--   usando list comprehension, para adicionar um sufixo às strings contidas numa lista. 
+
+
+--    3) Escreva uma função anosDeNascimento :: [Int] -> [Int] que receba uma lista de idades,
+--   selecione as que são maiores que 20 e, para cada uma das selecionadas, calcule o ano de nascimento correspondente (aproximado).
+--   Use a sintaxe de list comprehension.
