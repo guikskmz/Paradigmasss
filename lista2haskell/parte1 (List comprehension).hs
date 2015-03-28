08474efb7d0f54c4ae62510fcae0bf00e706f6e8
Guilherme Kaminski <gsantos@inf.ufsm.br>
2015-03-28 14:42:18 -0300

    ok
    ok

--- lista2haskell/parte1 (List comprehension).hs
+++ lista2haskell/parte1 (List comprehension).hs
@@ -8,11 +8,20 @@
 --    1) Escreva uma função geraPotencias :: Int -> [Int], que gere uma lista com as potências de 2
 --   com expoente de n até 0, onde n é o argumento para a função. Use a sintaxe de list comprehension. 
 
+geraPot :: Int -> [Int]
+geraPot x = [(2^x) | x <- [x,x-1..0]]
+
 
 --    2) Escreva uma função addSuffix :: String -> [String] -> [String]
 --   usando list comprehension, para adicionar um sufixo às strings contidas numa lista. 
+
+addSuffix :: String -> [String] -> [String]
+addSuffix s ls =  [(ls++s) | ls <- ls]
 
 
 --    3) Escreva uma função anosDeNascimento :: [Int] -> [Int] que receba uma lista de idades,
 --   selecione as que são maiores que 20 e, para cada uma das selecionadas, calcule o ano de nascimento correspondente (aproximado).
 --   Use a sintaxe de list comprehension.
+
+anosDeNascimento :: [Int] -> [Int]
+anosDeNascimento x = [(2015-x) | x <- x , x > 20]
