79e57a39f751ec3c1f66e636cd9423017f927048
Guilherme Kaminski <gsantos@inf.ufsm.br>
2015-04-13 07:10:22 -0300

    rapido
    té q deu tempo

---
+++ T3/Lista de logins/t3-logins.hs
@@ -0,0 +1,14 @@
+﻿-- Nome: Guilherme Kaminski dos Santos
+-- T3 - Haskell - log
+
+import Data.Char
+
+nusr :: String -> String
+nusr (x:xs) = map (toLower) ( x : reverse ( takeWhile (/= ' ')(reverse xs) ) )
+
+main :: IO ()
+main = do
+    strcontent <- readFile "nomes.csv"
+    let strlist = lines strcontent
+        strnew = map (\y -> y ++ "," ++ nusr y) strlist
+    writeFile "logins.csv" (unlines strnew)
