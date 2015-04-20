669744da4c40fb8f114c6f6ce1854cad51463fc4
Guilherme Kaminski <gsantos@inf.ufsm.br>
2015-04-20 05:58:12 -0300

    regras
    fim

--- T4/t4-assassinato.pl
+++ T4/t4-assassinato.pl
@@ -1,52 +1,52 @@
-% trabalho prolog para resolver um assassinato.
+% T4 - resolver um assassinato.
 
 esteve(pedro,sm,segunda).
 esteve(pedro,sm,terca).
 esteve(pedro,poa,quarta).
 esteve(pedro,sm,quinta).
-esteve(pedro,apartamento,sexta).
+esteve(pedro,apartamento,seXta).
 
 esteve(caren,poa,segunda).
 esteve(caren,poa,terca).
 esteve(caren,poa,quarta).
 esteve(caren,sm,quinta).
-esteve(caren,apartamento,sexta).
+esteve(caren,apartamento,seXta).
 
 esteve(henrique,apartamento,segunda).
 esteve(henrique,poa,terca).
 esteve(henrique,apartamento,quarta).
 esteve(henrique,apartamento,quinta).
-esteve(henrique,apartamento,sexta).
+esteve(henrique,apartamento,seXta).
 
 esteve(bia,apartamento,segunda).
 esteve(bia,poa,terca).
 esteve(bia,poa,quarta).
 esteve(bia,sm,quinta).
-esteve(bia,apartamento,sexta).
+esteve(bia,apartamento,seXta).
 
 esteve(adriano,apartamento,segunda).
 esteve(adriano,apartamento,terca).
 esteve(adriano,sm,quarta).
 esteve(adriano,apartamento,quinta).
-esteve(adriano,apartamento,sexta).
+esteve(adriano,apartamento,seXta).
 
 esteve(alice,apartamento,segunda).
 esteve(alice,poa,terca).
 esteve(alice,poa,quarta).
 esteve(alice,apartamento,quinta).
-esteve(alice,apartamento,sexta).
+esteve(alice,apartamento,seXta).
 
 esteve(bernardo,sm,segunda).
 esteve(bernardo,sm,terca).
 esteve(bernardo,poa,quarta).
 esteve(bernardo,sm,quinta).
-esteve(bernardo,apartamento,sexta).
+esteve(bernardo,apartamento,seXta).
 
 esteve(maria,apartamento,segunda).
 esteve(maria,sm,terca).
 esteve(maria,sm,quarta).
 esteve(maria,sm,quinta).
-esteve(maria,apartamento,sexta).
+esteve(maria,apartamento,seXta).
 
 insano(adriano).
 insano(maria).
@@ -66,16 +66,31 @@ relacionamento(henrique,maria).
 relacionamento(maria,adriano).
 relacionamento(adriano,caren).
 
-vitima(anita).
+relacionamento(bernardo,anita).
+relacionamento(caren,bernardo).
+relacionamento(pedro,anita).
+relacionamento(alice,pedro).
+relacionamento(henrique,alice).
+relacionamento(maria,henrique).
+relacionamento(adriano,maria).
+relacionamento(caren,adriano).
 
-assassino(x) :- motivo(x), acesso(x).
+motivo(X) :- ciumes(X,anita);insanidade(X);dinheiro(X).
 
-motivo(x) :- ciumes(x);insanidade(x);dinheiro(x).
-ciumes(x) :- relacionamento(x,anita),relacionamento(y,anita) %REVER<<<<
-insanidade(x):-insano(x).
-dinheiro(x):-pobre(x).
+ciumes(X,Y) :- relacionamento(X,A),relacionamento(Y,A).
+insanidade(X):-insano(X).
+dinheiro(X):-pobre(X).
 
-acesso(x)....
+acesso(X):- local(X), roubouchave(X), roubouarma(X).
+
+local(X):- esteve(X,apartamento,quinta); esteve(X,apartamento,seXta).
+roubouchave(X):- esteve(X,sm,segunda); esteve(X,poa,terca).
+roubouarma(X):- bastao(X);martelo(X).
+
+bastao(X):- esteve(X,poa,quinta); esteve(X,sm,quarta).
+martelo(X):- esteve(X,apartamento,quarta); esteve(X,apartamento,quinta).
+
+assassino(X):- motivo(X), acesso(X).
 
 
 
